b50cb83fcf9f64c7d5a5eb32289c19a0f90e7599
reulisson <reulisson.torres62@aluno.ifce.edu.br>
2024-09-02 10:52:09 -0300

    Exercícios 1 e 2 resolvidos

---
+++ TargetSistemas.Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe um número:");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (PertenceAFibonacci(numero))
+        {
+            Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool PertenceAFibonacci(int numero)
+    {
+        int a = 0;
+        int b = 1;
+
+        while (a <= numero)
+        {
+            if (a == numero)
+            {
+                return true;
+            }
+
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return false;
+    }
+}
---
+++ TargetSistemas.String/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma string:");
+        string input = Console.ReadLine();
+
+        int count = ContarOcorrenciasDeA(input);
+
+        if (count > 0)
+        {
+            Console.WriteLine($"A letra 'a' ocorre {count} vezes na string.");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' não foi encontrada na string.");
+        }
+    }
+
+    static int ContarOcorrenciasDeA(string input)
+    {
+        int count = 0;
+
+        foreach (char c in input)
+        {
+            if (char.ToLower(c) == 'a')
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
